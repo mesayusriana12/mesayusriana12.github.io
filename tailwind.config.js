286d429a67ca5273cc3bb5ac5d7013be867286e5
Muhammad Esa Yusriana <esa.yusriana@bts.id>
2025-04-27 11:07:35 +0700

    feat(migrate): tailwind init migrate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2c3e50',  // Add the custom color for primary
+      },
+    },
+  },
+  plugins: [],
+}
