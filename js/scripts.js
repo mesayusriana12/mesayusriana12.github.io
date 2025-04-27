547a0182f64f7f9b339625f450bcfc4c01f38d3a
Muhammad Esa Yusriana <esa.yusriana@bts.id>
2025-04-27 11:08:40 +0700

    Revert "feat(migrate): tailwind init migrate"
    This reverts commit d3e66d50ee9f8bd2b8ad5413ccf3d537e685365f.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,49 @@
 //
 // Scripts
-//
+// 
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', event => {
+
     // Navbar shrink function
-    const navbarToggler = document.getElementById('navbarToggler');
-    const navbarResponsive = document.getElementById('navbarResponsive');
-    const hamburgerIcon = document.getElementById('hamburgerIcon');
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
 
-    navbarToggler.addEventListener('click', () => {
-        // Toggle the menu visibility
-        navbarResponsive.classList.toggle('hidden');
+    };
 
-        // Toggle the hamburger animation (rotate to close)
-        hamburgerIcon.classList.toggle('rotate-90');
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
     });
 
     // Carousel Item HTML
@@ -26,7 +56,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         `<div class="carousel-item ${active ? 'active' : ''}">
             <img src="${src}" class="d-block w-100 rounded" alt="${alt}">
         </div>`;
-
+    
     // Carousel Indicator HTML
     /**
      * @param {string} target
@@ -34,9 +64,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
      * @returns {string}
      */
     carouselIndicator = (target, index = 0) =>
-        `<button type="button" data-bs-target="#${target}" data-bs-slide-to="${index}" aria-label="Slide ${index + 1
-        }" ${index === 0 ? 'class="active" aria-current="true"' : ''} ></button>`;
-
+        `<button type="button" data-bs-target="#${target}" data-bs-slide-to="${index}" aria-label="Slide ${index + 1}" ${index === 0 ? 'class="active" aria-current="true"' : ''} ></button>`;
+    
     // Badge HTML
     /**
      * @param {string} text
@@ -55,9 +84,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
         'https://wa.me/+6282118207806',
         'https://t.me/mesayusriana12',
         'https://www.instagram.com/mesayusriana12/',
-        'mailto:mesayusriana12@gmail.com',
+        'mailto:mesayusriana12@gmail.com'
     ];
-
+    
     socialMediaButton.forEach((button, index) => {
         button.addEventListener('click', () => {
             button.setAttribute('href', socialMediaLink[index]);
@@ -79,64 +108,51 @@ window.addEventListener('DOMContentLoaded', (event) => {
             },
         },
         rewind: true,
-    };
+    }
 
-    const splideFE = new Splide('#skillFrontend', {}).mount();
+    const splideFE = new Splide( '#skillFrontend', {}).mount();
     const splideBE = new Splide('#skillBackend', {}).mount();
     const splideTO = new Splide('#skillTools', {}).mount();
-
+    
     // Certificate Modal
     let certificateModal = document.getElementById('certificateModal');
-    certificateModal,
-        addEventListener('show.bs.modal', function (event) {
-            let related = event.relatedTarget.lastElementChild;
-            let modalTitle = document.getElementById('certificateModalTitle');
-            let modalPicture = document.getElementById('certificateModalPicture');
+    certificateModal, addEventListener('show.bs.modal', function (event) {
+        let related = event.relatedTarget.lastElementChild;
+        let modalTitle = document.getElementById('certificateModalTitle');
+        let modalPicture = document.getElementById('certificateModalPicture');
 
-            modalTitle.innerHTML = related.getAttribute('alt');
-            modalPicture.src = related.getAttribute('src');
-        });
+        modalTitle.innerHTML = related.getAttribute('alt');
+        modalPicture.src = related.getAttribute('src');
+    });
 
     // Project Modal
     let projectModal = document.getElementById('projectModal');
-    projectModal,
-        addEventListener('show.bs.modal', function (event) {
-            let htmlBadge = '';
-            let htmlCarouselItem = '';
-            let htmlCarouselIndicator = '';
+    projectModal, addEventListener('show.bs.modal', function (event) {
+        let htmlBadge = '';
+        let htmlCarouselItem = '';
+        let htmlCarouselIndicator = '';
 
-            let related = event.relatedTarget;
-            let modalTitle = document.getElementById('projectModalTitle');
-            let modalPicture = document.getElementById('projectModalPicture');
-            let modalCarouselIndicator = document.getElementById(
-                'projectModalCarouselIndicator'
-            );
-            let modalBadge = document.getElementById('projectModalBadge');
-            let modalDescription = document.getElementById('projectModalDescription');
+        let related = event.relatedTarget;
+        let modalTitle = document.getElementById('projectModalTitle');
+        let modalPicture = document.getElementById('projectModalPicture');
+        let modalCarouselIndicator = document.getElementById('projectModalCarouselIndicator');
+        let modalBadge = document.getElementById('projectModalBadge');
+        let modalDescription = document.getElementById('projectModalDescription');
 
-            related
-                .getAttribute('data-badge')
-                .split('|')
-                .forEach((badge) => {
-                    htmlBadge += badge ? badgeItem(badge) : '';
-                });
-
-            related
-                .getAttribute('data-picture')
-                .split('|')
-                .forEach((src, iterator) => {
-                    htmlCarouselItem += src
-                        ? carouselItem(`./assets/img/project/${src}`, iterator === 0)
-                        : '';
-                    htmlCarouselIndicator += src
-                        ? carouselIndicator('projectModalCarousel', iterator)
-                        : '';
-                });
-
-            modalTitle.innerHTML = related.getAttribute('data-title');
-            modalPicture.innerHTML = htmlCarouselItem;
-            modalCarouselIndicator.innerHTML = htmlCarouselIndicator;
-            modalBadge.innerHTML = htmlBadge;
-            modalDescription.innerHTML = related.getAttribute('data-description');
+        related.getAttribute('data-badge').split('|').forEach((badge) => {
+            htmlBadge += badge ? badgeItem(badge) : '';
         });
+
+        related.getAttribute('data-picture').split('|').forEach((src, iterator) => {
+            htmlCarouselItem += src ? carouselItem(`./assets/img/project/${src}`, iterator === 0) : '';
+            htmlCarouselIndicator += src ? carouselIndicator('projectModalCarousel', iterator) : '';
+        });
+        
+        modalTitle.innerHTML = related.getAttribute('data-title');
+        modalPicture.innerHTML = htmlCarouselItem;
+        modalCarouselIndicator.innerHTML = htmlCarouselIndicator;
+        modalBadge.innerHTML = htmlBadge;
+        modalDescription.innerHTML = related.getAttribute('data-description');
+    });
+
 });
